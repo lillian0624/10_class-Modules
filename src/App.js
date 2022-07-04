1e8672a0bbf6fbaf11209768e18f455d35a3b0fe
Lillian Yang <nopeyangli@163.com>
2022-07-04 22:25:33 +1000

    class module ref

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import User from './Components/User'
 
 export default class App extends Component {
   render() {
+   
     return (
-      <div className='app'><User/></div>
-    )
+      <div className='app'>
+        <User name="22" age={22} gender={'f'}/></div>
+    );
   }
-}
+} 
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react'
 
 export default class User extends Component {
+
+  divRef = React.createRef();
+
+state = {
+  count:0,
+  test:'hhh',
+  obj:{name:'sss', age:'22'}
+};
+
+clickHandler = ()=>{
+  // this.setState({count:10});
+  // this.setState(prevState =>{
+  //   return {
+  //     count:prevState +1 
+  //   }
+  // });
+  // this.setState({
+  //   obj:{...this.state.obj, name:'ddv'}
+  // });
+  console.log(this.divRef.current);
+}
+
   render() {
+    
+    // console.log(this.props);
+   
+
+
     return (
+      <div ref={this.divRef}>
+        <h1>{this.state.count}--{this.state.test}</h1>
+        <h2>{this.state.obj.name}---{this.state.obj.age}</h2>
+        <button onClick={this.clickHandler}>click</button>
+       
       <ul>
-          <li>name:22</li>
-          <li>age:13</li>
-          <li>gender：f</li>
+          <li>name:{this.props.name}</li>
+          <li>age:{this.props.age}</li>
+          <li>gender：{this.props.gender}</li>
       </ul>
+      </div>
     )
   }
 }
