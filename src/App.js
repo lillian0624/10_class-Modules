e23ec53e0e28d957e72a5a3beb7a4fe3653ce231
Lillian Yang <nopeyangli@163.com>
2022-07-04 20:54:00 +1000

    import

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import "./App.css";
-import { useState } from "react";
-import { useRef } from 'react';
+import React, { Component } from 'react'
+import './App.css'
+import User from './Components/User'
 
-let temp;
-const App = () => {
- 
-const h1Ref = useRef(1);
-// const h1Ref = {current:null};
-console.log(temp === h1Ref);
 
-const [count, setCount]= useState(1);
-temp = h1Ref;
-
- const clickHandler = ()=>{ 
-
-   console.log(h1Ref); 
-  // alert(h1Ref.current=== header);
-  // h1Ref.current.innerText='xx';
-//   const header = document.getElementById('header');
-// alert(header)
-// header.innerHTML = 'sss';
-
-};
-
-const countAddHandler = ()=>{
-  setCount(prevState=>prevState+1);
-};
-
-  return (
-    <div className={"app"} > 
-      <h1 id="header" ref={h1Ref}>
-       App{count}
-      </h1>
-      <button onClick={clickHandler}>+</button>
-      <button onClick={countAddHandler}>3</button>
-    </div>
-  );
-};
-
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'><User/></div>
+    )
+  }
+}
---
+++ src/Components/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class User extends Component {
+  render() {
+    return (
+      <ul>
+          <li>name:22</li>
+          <li>age:13</li>
+          <li>gender：f</li>
+      </ul>
+    )
+  }
+}
